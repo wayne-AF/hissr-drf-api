d83cc66108b71cbd393e749bcad3c0a19111ba66
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-02 15:43:32 +0000

    add further unit tests for FollowerDetail view

--- followers/tests.py
+++ followers/tests.py
@@ -18,7 +18,12 @@ class FollowerListViewTest(APITestCase):
         """
         User.objects.create_user(username='adam', password='pass')
 
-    def test_logged_out_user_cannot_follow_user(self)
+    def test_logged_out_user_cannot_follow_user(self):
+        """
+        Ensures logged-out user cannot follow user.
+        """
+        response = self.client.post('/followers/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 class FollowerDetailViewTests(APITestCase):
@@ -27,18 +32,53 @@ class FollowerDetailViewTests(APITestCase):
     """
 
     def setUp(self):
+        """
+        Creates three users and two follows of user 1 and user 2 at the start
+        of every test method.
+        """
+        adam = User.objects.create_user(username='adam', password='pass')
+        anna = User.objects.create_user(username='anna', password='pass')
+        bobby = User.objects.create_user(username='bobby', password='pass')
 
+        Follower.objects.create(owner=adam, followed_id=2)
+        Follower.objects.create(owner=anna, followed_id=3)
 
     def test_user_can_follow_other_user(self):
-
+        """
+        Ensures user can follow another user.
+        """
+        self.client.login(username='adam', password='pass')
+        response = self.client.post('/followers/', {'followed': 3})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_user_can_unfollow_user(self):
-
-
-    def test_user_can_retrieve_following_with_valid_id(self):
-
-
-    def test_user_cannot_retrieve_following_with_invalid_id(self):
-
+        """
+        Ensures user can unfollow a user.
+        """
+        self.client.login(username='adam', password='pass')
+        response = self.client.delete('/followers/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_user_cannot_perform_unfollow_for_another_user(self):
+        """
+        Ensures user cannot perform unfollow on behalf of another user.
+        """
+        self.client.login(username='adam', password='pass')
+        response = self.client.delete('/followers/2/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_can_retrieve_following_with_valid_id(self):
+        """
+        Ensures user can retrieve a following user with valid id.
+        """
+        self.client.login(username='adam', password='pass')
+        response = self.client.get('/followers/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_retrieve_following_with_invalid_id(self):
+        """
+        Ensures user cannot retrieve a following user with invalid id.
+        """
+        self.client.login(username='adam', password='pass')
+        response = self.client.get('/followers/1234/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
