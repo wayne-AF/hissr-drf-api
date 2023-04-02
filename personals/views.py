113b310b53f0057bd723a9e4ed04d7d5de62b58d
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-02 12:16:01 +0000

    add search and filter fields to PersonalList view

--- personals/views.py
+++ personals/views.py
@@ -9,10 +9,26 @@ from django_filters.rest_framework import DjangoFilterBackend
 class PersonalList(generics.ListCreateAPIView):
     """
     List Personal/Missed Connection posts or create one if user is logged in.
+    Users cannot like or comment on Missed Connection posts, so users cannot
+    filter posts by likes or comments.
     """
     serializer_class = PersonalSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Personal.objects.all()
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+        'country',
+        'city'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
