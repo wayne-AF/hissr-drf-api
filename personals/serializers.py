2fd1238fe4dae67d811498c0c772b03b60c8442b
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-02 12:07:07 +0000

    add connection_made field to PersonalSerializer

--- personals/serializers.py
+++ personals/serializers.py
@@ -6,35 +6,23 @@ from likes.models import Like
 
 class PersonalSerializer(CountryFieldMixin, serializers.ModelSerializer):
     """
-    Personals/Missed Connections cannot be commented on, only liked.
+    Personals/Missed Connections cannot be commented on or liked.
     Users can respond to the poster directly via message.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    # like_id = serializers.SerializerMethodField()
-    # likes_count = serializers.ReadOnlyField()
+    connection_made = serializers.BooleanField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-
-    # def get_like_id(self, obj):
-    #     user = self.context['request'].user
-    #     if user.is_authenticated:
-    #         like = Like.objects.filter(
-    #             owner=user, personal=obj
-    #         ).first()
-    #         return like.id if like else None
-    #     return None
 
     class Meta:
         model = Personal
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'country', 'city', 'title',
-            'content',
+            'content', 'connection_made'
         ]
-
-        # 'like_id', 'likes_count',
