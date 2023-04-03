13ec14479db13c4c2c6bf5f4afcfc68684281822
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-03 15:05:48 +0000

    set default renderer to JSON for production environment

--- hissr_drf_api/settings.py
+++ hissr_drf_api/settings.py
@@ -35,6 +35,11 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
