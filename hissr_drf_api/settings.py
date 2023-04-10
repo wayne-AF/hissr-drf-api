8649ca146ef94427e956a949b254cadf3d4051f9
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-10 18:01:24 +0000

    check settings.py for correct entries

--- hissr_drf_api/settings.py
+++ hissr_drf_api/settings.py
@@ -63,7 +63,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    ]
 
 
 # Application definition
