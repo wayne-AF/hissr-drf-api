fa16fcfadfc095f2d7fc9b323956edeab71b4dd2
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-11 14:01:31 +0000

    fix rest-framework authentication classes in settings.py

--- hissr_drf_api/settings.py
+++ hissr_drf_api/settings.py
@@ -27,7 +27,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASS': [(
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
