0471c8ff8d927469cbfb1a1d20830a6805e725f2
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-04-12 10:30:49 +0000

    update likes urls.py

--- likes/urls.py
+++ likes/urls.py
@@ -6,5 +6,5 @@ from likes import views
 
 urlpatterns = [
     path('likes/', views.LikeList.as_view()),
-    path('likes/<int:pk>', views.LikeDetail.as_view()),
+    path('likes/<int:pk>/', views.LikeDetail.as_view()),
 ]
